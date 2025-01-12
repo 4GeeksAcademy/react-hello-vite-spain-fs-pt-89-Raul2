8fe4f9ce1da208651345dc30931d2c5c7432b073
RaulSalas23 <rast2305@gmail.com>
2025-01-12 19:49:51 +0000

    landingReact-Raul

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,31 @@
 import React from "react";
+import Navbar from "./navbar";
+import Banner from "./banner"; 
+import Card from "./cardholder";
+import Footer from "./footer";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	return (    
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+<>
+			<Navbar/>
+			<Banner/>
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-12">
+						<Card/>
+					</div>
+				</div>
+				
+			</div>	
+			<Footer/>
+
+</>
+			
 	);
 };
 
---
+++ src/js/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-white text-center py-4">
+            <div className="container">
+                <p> <small>Copyright &copy; Your Website 2025</small></p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
